3a1122e3c7a276f2478579dfc809f297c601c9a3
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-07 14:32:12 +0100

    build: icons ok, eslint not

--- 3.14.1592_nsb8.3.3_sb8.4.7_eslint_unocss66.0.0/eslint.config.mjs
+++ 3.14.1592_nsb8.3.3_sb8.4.7_eslint_unocss66.0.0/eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+// import unocss from '@unocss/eslint-config/flat'
 
 export default withNuxt(
+  // unocss,
   // Your custom configs here
 )
